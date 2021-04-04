768e6b76a117d263eee1c3feac5c169b6c086e72
ndom91 <yo@ndo.dev>
2021-04-04 02:28:56 +0200

    fix: local build :wrench:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
