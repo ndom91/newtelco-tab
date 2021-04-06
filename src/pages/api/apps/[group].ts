c4dec16b1aeb6c2e1453d6d249b3c073e1a93041
ndom91 <yo@ndo.dev>
2021-04-06 02:05:53 +0200

    chore: cleanup :broom:

--- src/pages/api/apps/[group].ts
+++ src/pages/api/apps/[group].ts
@@ -1,11 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
 import Apps from './apps.json'
-import { getSession } from 'next-auth/client'
 
-export default async (req, res) => {
-  const session = await getSession({ req })
-  !session && res.status(401).end('not authenticated')
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const categoryApps = new Promise((resolve) => {
+    resolve(Apps.filter((category) => category.category.toLowerCase() === req.query.group))
+  })
 
-  const categoryApps = Apps.filter((category) => category.category.toLowerCase() === req.query.group)
-
-  res.send(categoryApps)
+  res.send(await categoryApps)
 }
