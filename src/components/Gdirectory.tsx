f02ed0a2f16430e1b779209800f21a2be617be9e
ndom91 <yo@ndo.dev>
2021-04-08 01:11:50 +0200

    feat: search to filter gdirectory :mag:

--- src/components/Gdirectory.tsx
+++ src/components/Gdirectory.tsx
@@ -8,57 +8,75 @@ const Gdirectory: React.FC = () => {
   const [session] = useSession()
   const [people, setPeople] = useState({
     data: [],
+    filteredPeople: [],
     loading: false,
     loginRequired: false,
     error: '',
   })
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!session) {
-        setPeople({ ...people, loading: false, loginRequired: true })
-        return true
-      }
-
-      setPeople({ ...people, loading: true, loginRequired: false })
-      try {
-        const response = await fetch('/api/directory')
-
-        if (!response.ok) {
-          throw new Error(`${response.status} - ${response.statusText}`)
-        }
-
-        const data = await response.json()
-        const filteredPeople = data.people
-          .reduce((acc, person) => {
-            if (person?.phoneNumbers) {
-              acc.push({
-                name: person.names[0].displayName,
-                phones: person.phoneNumbers?.map((phone) => phone.canonicalForm) ?? [],
-                email: person.emailAddresses?.[0].value,
-                position: person.organizations?.[0].title,
-                department: person.organizations?.[0].department,
-                img: person.photos?.[0].url,
-              })
-            }
-            return acc
-          }, [])
-          .sort((a, b) => {
-            if (a.name > b.name) {
-              return 1
-            }
-            return -1
-          })
-
-        setPeople({ ...people, data: filteredPeople, loading: false, loginRequired: false })
-      } catch (e) {
-        void signIn('google')
-        setPeople({ ...people, loading: false, loginRequired: true, error: e.message })
-      }
+  const fetchData = async () => {
+    if (!session) {
+      setPeople({ ...people, loading: false, loginRequired: true })
+      return true
     }
 
+    setPeople({ ...people, loading: true, loginRequired: false })
+    try {
+      const response = await fetch('/api/directory')
+
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`)
+      }
+
+      const data = await response.json()
+      const filteredPeople = data.people
+        .reduce((acc, person) => {
+          if (person?.phoneNumbers) {
+            acc.push({
+              name: person.names[0].displayName,
+              phones: person.phoneNumbers?.map((phone) => phone.canonicalForm) ?? [],
+              email: person.emailAddresses?.[0].value,
+              position: person.organizations?.[0].title,
+              department: person.organizations?.[0].department,
+              img: person.photos?.[0].url,
+            })
+          }
+          return acc
+        }, [])
+        .sort((a, b) => {
+          if (a.name > b.name) {
+            return 1
+          }
+          return -1
+        })
+
+      setPeople({ ...people, filteredPeople, data: filteredPeople, loading: false, loginRequired: false })
+    } catch (e) {
+      void signIn('google')
+      setPeople({ ...people, loading: false, loginRequired: true, error: e.message })
+    }
+  }
+
+  useEffect(() => {
     void fetchData()
   }, [session])
+
+  const handleSearch = (input) => {
+    const value = input.target.value.toLowerCase()
+
+    if (value) {
+      const filteredPeople = people.data.filter((person) => person.name.toLowerCase().includes(value))
+      setPeople({
+        loading: false,
+        loginRequired: false,
+        data: [...people.data],
+        filteredPeople,
+        error: '',
+      })
+    } else {
+      void fetchData()
+    }
+  }
 
   return (
     <div tw="shadow-lg rounded-xl p-4 bg-gray-900 relative overflow-hidden w-full" css="max-height: 650px">
@@ -83,12 +101,35 @@ const Gdirectory: React.FC = () => {
           <span tw="leading-9 ml-2">Contact Info</span>
         </div>
       </div>
+      <div tw="mx-8 flex relative focus-within:(ring-4 ring-newtelco-500 ring-opacity-20) transition-shadow ease-in-out duration-300 rounded-lg">
+        <span tw="rounded-l-lg inline-flex  items-center px-3 border-t bg-gray-800 border-l border-b border-gray-800 text-gray-500 shadow-sm text-sm">
+          <svg
+            height="15"
+            width="15"
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+        </span>
+        <input
+          type="text"
+          id="searchUsers"
+          onChange={(input) => handleSearch(input)}
+          tw=" rounded-r-lg flex-1 appearance-none border border-gray-800 w-full py-2 px-4 bg-gray-800 text-gray-200 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:border-transparent"
+          name="search"
+          placeholder="Search"
+        />
+      </div>
       {people.loading ? (
         <Loader />
       ) : (
         <div tw="flex flex-col justify-between p-4 m-4 overflow-y-scroll space-y-4" css="max-height: 550px">
-          {people.data.length > 0 ? (
-            people.data.map((person) => <UserCard key={person.id} person={person} />)
+          {people.filteredPeople.length > 0 ? (
+            people.filteredPeople.map((person) => <UserCard key={person.id} person={person} />)
           ) : (
             <div tw="flex flex-col justify-center align-middle space-y-4 h-48 text-center font-thin">
               {people.loginRequired ? (
